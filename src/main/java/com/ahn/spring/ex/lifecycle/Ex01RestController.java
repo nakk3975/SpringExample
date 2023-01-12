7414de136195b05843d72f1c8ac0573050041ba9
AHN <dsfs3975@naver.com>
2023-01-12 19:37:39 +0900

    [안재원]  - lifecycle 예제 추가

---
+++ src/main/java/com/ahn/spring/ex/lifecycle/Ex01RestController.java
@@ -0,0 +1,42 @@
+package com.ahn.spring.ex.lifecycle;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ahn.spring.ex.lifecycle.model.Person;
+
+@RestController		// @Controller + @ResponseBody
+@RequestMapping("/lifecycle/ex01")
+public class Ex01RestController {
+	
+	@RequestMapping("/3")
+	public String stringResponse() {
+		return "안녕하세요 <br> RestController 입니다.";
+	}
+	
+	// 직접 만든 클래스의 객체 리턴
+	@RequestMapping("/4")
+	public Person objectResponse() {
+		Person person = new Person();
+		person.setName("김인규");
+		person.setAge(28);
+		
+		return person;
+	}
+	
+	// status code를 포함한 response 
+	@RequestMapping("/5")
+	public ResponseEntity<Person> entityResponse() {
+		Person person = new Person();
+		person.setName("김인규");
+		person.setAge(28);
+		
+		// http status code
+		ResponseEntity<Person> entity = new ResponseEntity(person, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return entity;
+	}
+	
+}
